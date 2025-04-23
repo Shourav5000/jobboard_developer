33ce13cd733069636154855c559e865f4b1b288c
Shourav Mandal <69061713+Shourav5000@users.noreply.github.com>
2025-04-23 11:24:38 -0400

    Updates

--- jobboard-next/next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'export',
-  images: { unoptimized: true },
-  basePath: "/jobboard_developer",
-  assetPrefix: "/jobboard_developer"
-};
-
-module.exports = nextConfig;
